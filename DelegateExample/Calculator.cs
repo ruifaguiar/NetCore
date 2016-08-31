f5a6d15716baeda4b4985bc5bb9865a3d172fccd
Rui Aguiar <rui.aguiar@faroeurope.com>
2016-08-31 17:50:33 +0100

    fix

---
+++ DelegateExample/Calculator.cs
@@ -0,0 +1,15 @@
+namespace NetCore.DelegateExample
+{
+    public class Calculator
+    {
+        public int Add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+        public int Multiply(int val1, int val2)
+        {
+            return val1 * val2;
+        }
+    }
+}
